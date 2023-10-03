f50fd6450283605ca003a29c2395d4f5f070427e
Frandy <frandyfancy@gmail.com>
2023-10-03 18:32:27 +0800

    first push

---
+++ lib/home.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home ({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Container(
+              width: double.infinity,
+              height: 240,
+              decoration: const BoxDecoration(
+                color: Color(0xff368983),
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(20),
+                  bottomRight: Radius.circular(20),
+                ),
+              ),
+              child: Stack(
+                children: [
+                  Positioned(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(7),
+                      child: Container(
+                        height: 40,
+                        width: 40,
+                        color: const Color.fromRGBO(250, 250, 250, 0.1),
+                        child: const Icon(
+                          Icons.notification_add_outlined,
+                          size: 30,
+                          color: Colors.white,
+                          ),
+                      ),
+                    )
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),        
+      ),
+    );
+  }
+}
